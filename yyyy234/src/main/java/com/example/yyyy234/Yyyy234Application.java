402737b73572530f07e28aa9bbe21457434dc779
Shoaib <soabghanchi9627@gmail.com>
2024-10-19 14:04:11 +0530

    inital commit

---
+++ yyyy234/src/main/java/com/example/yyyy234/Yyyy234Application.java
@@ -0,0 +1,13 @@
+package com.example.yyyy234;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yyyy234Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yyyy234Application.class, args);
+	}
+
+}
